a0cc57942bf31fdb941cf5c2880d4647984c93e4
s-takahashijp <shallow.e1621@gmail.com>
2025-11-16 12:55:25 +0900

    11/16 add param for spel.

--- launch/gps_ichimill.launch.py
+++ launch/gps_ichimill.launch.py
@@ -1,18 +1,36 @@
 import os
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 def generate_launch_description():
 
+    # launchコマンドの引数を宣言
+    port_arg = DeclareLaunchArgument(
+        'port', default_value='/dev/GNSS_SERIAL', description='Serial port for F9P receiver'
+    )
+    baud_arg = DeclareLaunchArgument(
+        'baud', default_value='230400', description='Baud rate for F9P receiver'
+    )
+    debug_arg = DeclareLaunchArgument(
+        'debug', default_value='True', description='Enable debug logging'
+    )
+
     return LaunchDescription([
+        # 宣言した引数をLaunchDescriptionに追加
+        port_arg,
+        baud_arg,
+        debug_arg,
+
         Node(
             package='f9p_ichimill',
             executable='f9p_driver',
             name='f9p_driver',
             parameters=[
-                {'port': '/dev/GNSS_SERIAL'},
-                {'baud': 230400},
-                {'debug': True},
+                {'port': LaunchConfiguration('port')},
+                {'baud': LaunchConfiguration('baud')},
+                {'debug': LaunchConfiguration('debug')},
             ]
         ),
         Node(
@@ -26,7 +44,7 @@ def generate_launch_description():
                 {'port': 2101},
                 {'host': 'ntrip.ales-corp.co.jp'},
                 {'mountpoint': '32M7NHS'},
-                {'debug': True},
+                {'debug': LaunchConfiguration('debug')},
             ]
         ),
         Node(
