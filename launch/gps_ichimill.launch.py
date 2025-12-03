9199c55d1735b6a52c0998a43ac6db544f59b3d2
f-wada <f-wada@hokuyo-aut.co.jp>
2025-12-03 17:08:05 +0900

    fix launch file

--- launch/gps_ichimill.launch.py
+++ launch/gps_ichimill.launch.py
@@ -13,15 +13,19 @@ def generate_launch_description():
     baud_arg = DeclareLaunchArgument(
         'baud', default_value='230400', description='Baud rate for F9P receiver'
     )
-    debug_arg = DeclareLaunchArgument(
-        'debug', default_value='True', description='Enable debug logging'
+    debug_arg_ichimill = DeclareLaunchArgument(
+        'debug_ichimill', default_value='True', description='Enable debug logging'
+    )
+    debug_arg_driver = DeclareLaunchArgument(
+        'debug_driver', default_value='False', description='Enable debug logging'
     )
 
     return LaunchDescription([
         # 宣言した引数をLaunchDescriptionに追加
         port_arg,
         baud_arg,
-        debug_arg,
+        debug_arg_ichimill,
+        debug_arg_driver,
 
         Node(
             package='f9p_ichimill',
@@ -30,7 +34,7 @@ def generate_launch_description():
             parameters=[
                 {'port': LaunchConfiguration('port')},
                 {'baud': LaunchConfiguration('baud')},
-                {'debug': LaunchConfiguration('debug')},
+                {'debug': LaunchConfiguration('debug_driver')},
             ]
         ),
         Node(
@@ -44,7 +48,7 @@ def generate_launch_description():
                 {'port': 2101},
                 {'host': 'ntrip.ales-corp.co.jp'},
                 {'mountpoint': '32M7NHS'},
-                {'debug': LaunchConfiguration('debug')},
+                {'debug': LaunchConfiguration('debug_ichimill')},
             ]
         ),
         Node(
